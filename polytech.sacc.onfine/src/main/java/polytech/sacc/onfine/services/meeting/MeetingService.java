2c65c35ead4db04ff7183fa6ddaedb7838f5fc18
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-11-13 16:55:49 +0100

    Meeting service stored in NoSQl BDD

--- polytech.sacc.onfine/src/main/java/polytech/sacc/onfine/services/meeting/MeetingService.java
+++ polytech.sacc.onfine/src/main/java/polytech/sacc/onfine/services/meeting/MeetingService.java
@@ -1,6 +1,8 @@
 package polytech.sacc.onfine.services.meeting;
 
 import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.cloud.datastore.*;
+import polytech.sacc.onfine.entity.Gps;
 import polytech.sacc.onfine.entity.Meeting;
 
 import javax.servlet.ServletException;
@@ -17,9 +19,40 @@ public class MeetingService extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Meeting meeting = new Gson().fromJson(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), Meeting.class);
         System.out.println(meeting);
-        //TODO do you bail
+
+        Datastore datastore = connectToDatastore();
+        IncompleteKey key = getDatastoreKey(datastore);
+
+        FullEntity<IncompleteKey> newMeeting = FullEntity.newBuilder(key)
+                .set("meeting_sha1", meeting.getSha1())
+                .set("meeting_sha1Met", meeting.getSha1Met())
+                .set("meeting_gps_latitude", meeting.getGps().getLatitude())
+                .set("meeting_gps_longitude", meeting.getGps().getLongitude())
+                .set("meeting_timestamp", meeting.getTimestamp())
+                .build();
+
         resp.setStatus(HttpServletResponse.SC_CREATED);
-        resp.getWriter().print("Ok");
+        resp.getWriter().print(new Gson().toJson(parseEntity(datastore.add(newMeeting))));
+    }
+
+    private Meeting parseEntity(Entity entity){
+        Gps tmp = new Gps((float) entity.getDouble("meeting_gps_latitude"), (float) entity.getDouble("meeting_gps_longitude"));
+        return new Meeting(
+                entity.getString("meeting_sha1"),
+                entity.getString("meeting_sha1Met"),
+                tmp,
+                entity.getString("meeting_timestamp")
+        );
+    }
+
+
+    private Datastore connectToDatastore(){
+        return DatastoreOptions.getDefaultInstance().getService();
+    }
+
+    private IncompleteKey getDatastoreKey(Datastore datastore){
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Meeting");
+        return keyFactory.setKind("Meeting").newKey();
     }
 }
 
