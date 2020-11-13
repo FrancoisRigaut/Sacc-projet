550d7c1c026af312650ce0aeb520ee54d820b3e9
Pierre-Antoine Boulat <pierre-antoine.boulat@etu.unice.fr>
2020-11-13 16:55:21 +0100

    post user register done

--- polytech.sacc.onfine/src/main/java/polytech/sacc/onfine/services/user/UserService.java
+++ polytech.sacc.onfine/src/main/java/polytech/sacc/onfine/services/user/UserService.java
@@ -2,6 +2,11 @@ package polytech.sacc.onfine.services.user;
 
 import com.google.appengine.repackaged.com.google.gson.Gson;
 import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import org.apache.commons.codec.digest.DigestUtils;
 import polytech.sacc.onfine.tools.Utils;
 import polytech.sacc.onfine.entity.User;
 import polytech.sacc.onfine.entity.exception.WrongArgumentException;
@@ -60,9 +65,29 @@ public class UserService extends HttpServlet {
         JsonObject jsonObject = new Gson().fromJson(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), JsonObject.class);
         String phone = jsonObject.get("phone").getAsString();
         System.out.println(phone);
-        //TODO do you bail
-        resp.setStatus(HttpServletResponse.SC_CREATED);
-        resp.getWriter().print("Ok");
+        String sha1 = DigestUtils.sha1Hex(phone);
+        System.out.println(sha1);
+
+        //TODO store in datastore
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+        String kind = "User";
+        Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(sha1);
+
+        try {
+            Entity retrieved = datastore.get(taskKey);
+            System.out.printf("Retrieved %s%n", retrieved.getKey());
+            //TODO error
+            resp.setStatus(HttpServletResponse.SC_CONFLICT);
+            resp.getWriter().print("User already exists");
+        } catch (Exception e) {
+            Entity task = Entity.newBuilder(taskKey)
+                    .build();
+            datastore.add(task);
+            System.out.printf("Saved %s : %s", task.getKey().getId(), task.getKey().getName());
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            resp.getWriter().print("User created");
+        }
     }
 
     private void handleSetPoiUser(HttpServletRequest req, HttpServletResponse resp) throws Exception{
